2a68e4c055c1611818d551b5e70518e0f9711aa3
Yashwanth M <byg2281999@gmail.com>
2025-11-05 23:47:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function mincost(arr)
 { 
+	let totalcost = 0;
+	while (arr.length>1){
+		arr.sort((a, b) => a - b);
+		let first = arr[0];
+		let second = arr[1];
+		let newrope = first + second ;
+		totalcost += newrope;
+		arr.splice(0,2);
+		arr.push(newrope);
+	}
 //write your code here
 // return the min cost
   
